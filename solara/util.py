266c03366d9279ec77a05bcd83440c8e1aec95f4
Mario Buikhuizen <mariobuikhuizen@gmail.com>
2022-03-18 11:14:39 +0100

    feat: add function for loading a file as dataURL

---
+++ solara/util.py
@@ -0,0 +1,7 @@
+import base64
+
+
+def load_file_as_data_url(file_name, mime):
+    with open(file_name, 'rb') as f:
+        data = f.read()
+    return f'data:{mime};base64,' + base64.b64encode(data).decode('utf-8')
