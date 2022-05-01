5c89f8178f81bf54b0e7431e3fb48bcea9ebe7fa
Maarten A. Breddels <maartenbreddels@gmail.com>
2022-05-01 15:22:14 +0200

    feat: make watchdog optional (not needed for pyiodide)

--- solara/server/reload.py
+++ solara/server/reload.py
@@ -5,84 +5,117 @@ import logging
 import os
 import sys
 import threading
-from typing import Callable, Dict, List, Set
+from typing import Callable, Dict, List, Set, Type
 
-from watchdog.events import FileSystemEventHandler
-from watchdog.observers import Observer
-
+NO_WATCHDOG = False
+try:
+    from watchdog.events import FileSystemEventHandler
+    from watchdog.observers import Observer
+except ImportError:
+    pass
+    NO_WATCHDOG = True
 logger = logging.getLogger("solara.server.reload")
 
 
-class Watcher(FileSystemEventHandler):
+class Watcher:
     def __init__(self, files, on_change: Callable[[str], None]):
-        self.on_change = on_change
-        self.observers: List[Observer] = []
-        self.directories: Set[str] = set()
-        self.files: List[str] = []
-        self.mtimes: Dict[str, float] = dict()
-        for file in files:
-            self.add_file(file)
+        pass
 
     def close(self):
-        for o in self.observers:
-            o.unschedule_all()
-            o.stop()
-        # this makes testing slow
-        # for o in self.observers:
-        #     o.join()
+        pass
 
     def add_file(self, file):
-        file = os.path.abspath(os.path.realpath(file))
-        if file not in self.files:
-            logger.info("Watching file %s", file)
-            if file in self.files:
-                raise RuntimeError(f"{file} was already added")
-            self.files.append(file)
-            self._watch_file(file)
+        pass
 
-    def _watch_file(self, file):
-        self.mtimes[file] = os.path.getmtime(file)
-        directory = os.path.realpath(os.path.dirname(file))
-        self.watch_directory(directory)
 
-    def watch_directory(self, directory):
-        if directory not in self.directories:
-            logger.debug("Watching directory %s", directory)
-            observer = Observer()
-            observer.schedule(self, directory, recursive=False)
-            observer.start()
-            self.observers.append(observer)
-            self.directories.add(directory)
+class WatcherDummy:
+    def __init__(self, files, on_change: Callable[[str], None]):
+        pass
 
-    def on_modified(self, event):
-        super(Watcher, self).on_modified(event)
-        logger.info("Watch event: %s", event)
-        print("EVENT", event)
-        if not event.is_directory:
-            if event.src_path in self.files:
-                mtime_new = os.path.getmtime(event.src_path)
-                changed = mtime_new > self.mtimes[event.src_path]
-                self.mtimes[event.src_path] = mtime_new
-                if changed:
-                    logger.debug("File modified: %s", event.src_path)
-                    try:
-                        self.on_change(event.src_path)
-                    except:  # noqa
-                        # we are in the watchdog thread here, all we can do is report
-                        # and continue running (otherwise reload stops working)
-                        logger.exception("Error while executing on change handler")
+    def close(self):
+        pass
+
+    def add_file(self, file):
+        pass
+
+
+if NO_WATCHDOG:
+    WatcherType: Type[Watcher] = WatcherDummy  # type: ignore
+
+else:
+
+    class WatcherWatchdog(FileSystemEventHandler):
+        def __init__(self, files, on_change: Callable[[str], None]):
+            self.on_change = on_change
+            self.observers: List[Observer] = []
+            self.directories: Set[str] = set()
+            self.files: List[str] = []
+            self.mtimes: Dict[str, float] = dict()
+            for file in files:
+                self.add_file(file)
+
+        def close(self):
+            for o in self.observers:
+                o.unschedule_all()
+                o.stop()
+            # this makes testing slow
+            # for o in self.observers:
+            #     o.join()
+
+        def add_file(self, file):
+            file = os.path.abspath(os.path.realpath(file))
+            if file not in self.files:
+                logger.info("Watching file %s", file)
+                if file in self.files:
+                    raise RuntimeError(f"{file} was already added")
+                self.files.append(file)
+                self._watch_file(file)
+
+        def _watch_file(self, file):
+            self.mtimes[file] = os.path.getmtime(file)
+            directory = os.path.realpath(os.path.dirname(file))
+            self.watch_directory(directory)
+
+        def watch_directory(self, directory):
+            if directory not in self.directories:
+                logger.debug("Watching directory %s", directory)
+                observer = Observer()
+                observer.schedule(self, directory, recursive=False)
+                observer.start()
+                self.observers.append(observer)
+                self.directories.add(directory)
+
+        def on_modified(self, event):
+            super(WatcherWatchdog, self).on_modified(event)
+            logger.info("Watch event: %s", event)
+            print("EVENT", event)
+            if not event.is_directory:
+                if event.src_path in self.files:
+                    mtime_new = os.path.getmtime(event.src_path)
+                    changed = mtime_new > self.mtimes[event.src_path]
+                    self.mtimes[event.src_path] = mtime_new
+                    if changed:
+                        logger.debug("File modified: %s", event.src_path)
+                        try:
+                            self.on_change(event.src_path)
+                        except:  # noqa
+                            # we are in the watchdog thread here, all we can do is report
+                            # and continue running (otherwise reload stops working)
+                            logger.exception("Error while executing on change handler")
+                    else:
+                        logger.debug("File reported modified, but mtime did not change: %s", event.src_path)
+                        print("File reported modified, but mtime did not change: ", event.src_path)
                 else:
-                    logger.debug("File reported modified, but mtime did not change: %s", event.src_path)
-                    print("File reported modified, but mtime did not change: ", event.src_path)
-            else:
-                logger.debug("Ignore file modification: %s", event.src_path)
+                    logger.debug("Ignore file modification: %s", event.src_path)
+
+    WatcherType = WatcherWatchdog
 
 
 class Reloader:
     def __init__(self, on_change: Callable[[str], None] = None) -> None:
         self.watched_modules: Set[str] = set()
         self.on_change = on_change
-        self.watcher = Watcher([], self._on_change)
+        self.watcher = WatcherType([], self._on_change)
         self.requires_reload = False
         self.reload_event_next = threading.Event()
 
