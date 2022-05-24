widgetti/solara

commit 71aad0d906d66cca0aa99d9a5a5255f3fd5d9410

feat: add parent directory to sys.path, similar to running a python script
Otherwise we cannot do 'import somelocalfile'
when somelocalfile.py is in the same directory as the app.py

solara/server/app.py
@@ -5,6 +5,7 @@ import importlib.util
 import logging
 import os
 import pickle
+import sys
 import threading
 from enum import Enum
 from pathlib import Path
@@ -166,6 +167,9 @@ class AppScript:
                 assert spec is not None
                 assert spec.origin is not None
                 self.path = Path(spec.origin)
+        working_directory = str(self.path.parent)
+        if working_directory not in sys.path:
+            sys.path.insert(0, working_directory)
 
         # this might be useful for development
         # but requires reloading of react in solara iself

solara/__init__.py
@@ -9,7 +9,6 @@ from .datatypes import *
 from .hooks import *
 from .memoize import memoize
 from .components import *
-from .widgets import watch as auto_reload_vue
 
 github_url = "https://github.com/widgetti/solara"
 git_branch = "master"

commit a0a8c4c60daf0550b3625702636b7a908f7514bb

fix: no need to change sys.path for apps wich are modules
Fixes issue introduced in
71aad0d906d66cca0aa99d9a5a5255f3fd5d9410

For instance, having a bqplot submodule would shadow the bqplot package

solara/server/app.py
@@ -167,9 +167,11 @@ class AppScript:
                 assert spec is not None
                 assert spec.origin is not None
                 self.path = Path(spec.origin)
-        working_directory = str(self.path.parent)
-        if working_directory not in sys.path:
-            sys.path.insert(0, working_directory)
+        # this is not expected for modules, similar to `python script.py and python -m package.mymodule`
+        if self.type in [AppType.SCRIPT, AppType.NOTEBOOK]:
+            working_directory = str(self.path.parent)
+            if working_directory not in sys.path:
+                sys.path.insert(0, working_directory)
 
         # this might be useful for development
         # but requires reloading of react in solara iself