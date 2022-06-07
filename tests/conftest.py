33abac7bab547ca32ee6372ff40f1aed5f93b060
Maarten A. Breddels <maartenbreddels@gmail.com>
2022-06-07 21:30:33 +0200

    fix: tests could fail because patch was not run

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,12 +3,17 @@ import sys
 
 import pytest
 
-from solara.server import kernel
+from solara.server import kernel, patch
 from solara.server.app import AppContext
 
 
+@pytest.fixture(scope="session")
+def solara_patched():
+    patch.patch()
+
+
 @pytest.fixture(autouse=True)
-def app_context():
+def app_context(solara_patched):
     kernel_shared = kernel.Kernel()
     context = AppContext(id="1", kernel=kernel_shared, control_sockets=[], widgets={}, templates={})
     try:
