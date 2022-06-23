8c1cf825663859eb50b7691fb407801765329d38
Mario Buikhuizen <mariobuikhuizen@gmail.com>
2022-06-23 20:20:28 +0200

    temp: change release branch

--- release.sh
+++ release.sh
@@ -3,4 +3,4 @@ set -e -o pipefail
 # usage: ./release minor -n
 version=$(bump2version --dry-run --list $* | grep new_version | sed -r s,"^.*=",,)
 echo Version tag v$version
-bumpversion $* --verbose && git push upstream master v$version
+bumpversion $* --verbose && git push upstream feat_offline_mode v$version
