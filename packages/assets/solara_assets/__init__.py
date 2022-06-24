63ed7c6242b5b4c5f6dd65de94192ed04e330153
Mario Buikhuizen <mariobuikhuizen@gmail.com>
2022-06-24 12:00:44 +0200

    Bump version: 0.4.2a0 → 0.4.3a0

--- packages/assets/solara_assets/__init__.py
+++ packages/assets/solara_assets/__init__.py
@@ -1,2 +1,2 @@
 "CDN assets for Solara"
-__version__ = "0.4.2a0"
+__version__ = "0.4.3a0"
--- solara/__init__.py
+++ solara/__init__.py
@@ -1,5 +1,5 @@
 """Build webapps using IPywidgets"""
-__version__ = "0.4.2a0"
+__version__ = "0.4.3a0"
 # isort: skip_file
 
 from . import util
