f769319d2e9e8a0e9b833eb12d936cfab64d67c4
Maarten A. Breddels <maartenbreddels@gmail.com>
2022-09-12 13:42:31 +0200

    fix(assets): do not put content under 'packages' subdirectory.

--- packages/assets/hatch_build.py
+++ packages/assets/hatch_build.py
@@ -20,12 +20,27 @@ packages = [
 
 def npm_pack(base_cache_dir: pathlib.Path, package: str, version: str):
     with tempfile.TemporaryDirectory() as temp_dir_name:
-        subprocess.check_call(f"npm pack {package}@{version}", cwd=temp_dir_name, shell=True)
-        package_file_name = package
-        if package.startswith("@"):
-            package_file_name = package[1:].replace("/", "-")
-        subprocess.check_call(f"tar xzf {package_file_name}-{version}.tgz", cwd=temp_dir_name, shell=True)
-        shutil.move(str(pathlib.Path(temp_dir_name) / "package"), str(base_cache_dir / f"{package}@{version}"))
+        target_directory = base_cache_dir / f"{package}@{version}"
+        if target_directory.exists():
+            if (target_directory / "package.json").exists():
+                print(f"package: {package} already downloaded, skipping")  # noqa
+                return
+            else:
+                # if the directory exists and we 'move', the directory will be
+                # moved to the 'package' subdirectory, so remove it first
+                target_directory.unlink()
+        try:
+            subprocess.check_call(f"npm pack {package}@{version}", cwd=temp_dir_name, shell=True)
+            package_file_name = package
+            if package.startswith("@"):
+                package_file_name = package[1:].replace("/", "-")
+            subprocess.check_call(f"tar xzf {package_file_name}-{version}.tgz", cwd=temp_dir_name, shell=True)
+            shutil.move(str(pathlib.Path(temp_dir_name) / "package"), str(target_directory))
+        except Exception:
+            # creating for convenience, to unpack a tarball
+            # in the right directory
+            target_directory.mkdir(exist_ok=True, parents=True)
+            raise
 
 
 def download_cdn(cache_dir):
