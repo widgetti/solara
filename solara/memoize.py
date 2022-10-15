a8822828ac09e3e10ce8978e645bbc03ca642e77
Maarten A. Breddels <maartenbreddels@gmail.com>
2022-10-15 14:04:48 +0200

    fix: if sqlite3 does not work, have memoize be a dummy decorator

--- solara/memoize.py
+++ solara/memoize.py
@@ -1,6 +1,21 @@
+import logging
+
 from diskcache import Cache
 
-cache = Cache()
+logger = logging.getLogger("solara.memoize")
 
+try:
+    cache = Cache()
+except:  # noqa
+    # on digital ocean for example, we get sqlite3.OperationalError: disk I/O error
+    logger.warning("Could not create diskcache, memoization will not work")
 
-memoize = cache.memoize
+    def identity(name=None, typed=False, expire=None, tag=None, ignore=()):
+        def f(func):
+            return func
+
+        return f
+
+    memoize = identity
+else:
+    memoize = cache.memoize
