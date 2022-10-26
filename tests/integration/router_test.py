4edf564cbff46236d6f33e6e95f3621a2776a6f7
Maarten A. Breddels <maartenbreddels@gmail.com>
2022-10-26 21:53:02 +0200

    more tests (router_test)

--- tests/integration/router_test.py
+++ tests/integration/router_test.py
@@ -1,37 +1,34 @@
-# import playwright
-# import playwright.sync_api
+import playwright
+import playwright.sync_api
 
 
-# def test_docs_basics(page: playwright.sync_api.Page, solara_server, solara_app):
-#     # with screenshot_on_error(page, 'tmp/test_docs_basics.png'):
-#     with solara_app("solara.website.pages"):
-#         # page.goto(solara_server.base_url)
-#         # assert page.title() == "Hello from Solara ☀️"
-#         # page.locator('div[role="tab"]:has-text("Demo")').click()
-#         page.goto(solara_server.base_url + "/api/use_route/fruit/banana")
-#         page.locator("text=You chose banana").wait_for()
-#         page.locator('button:has-text("kiwi")').click()
-#         page.locator("text=You chose kiwi").wait_for()
-#         page.locator('button:has-text("apple")').click()
-#         page.locator("text=You chose apple").wait_for()
-#         # back to kiwi
-#         page.go_back()
-#         page.locator("text=You chose kiwi").wait_for()
-#         # back to banana
-#         page.go_back()
-#         page.locator("text=You chose banana").wait_for()
+def test_docs_basics(page_session: playwright.sync_api.Page, solara_server, solara_app):
+    # with screenshot_on_error(page, 'tmp/test_docs_basics.png'):
+    with solara_app("solara.website.pages"):
+        page_session.goto(solara_server.base_url + "/api/use_route/fruit/banana")
+        page_session.locator("text=You chose banana").wait_for()
+        page_session.locator('button:has-text("kiwi")').click()
+        page_session.locator("text=You chose kiwi").wait_for()
+        page_session.locator('button:has-text("apple")').click()
+        page_session.locator("text=You chose apple").wait_for()
+        # back to kiwi
+        page_session.go_back()
+        page_session.locator("text=You chose kiwi").wait_for()
+        # back to banana
+        page_session.go_back()
+        page_session.locator("text=You chose banana").wait_for()
 
-#         # forward to kiwi
-#         page.go_forward()
-#         page.locator("text=You chose kiwi").wait_for()
+        # forward to kiwi
+        page_session.go_forward()
+        page_session.locator("text=You chose kiwi").wait_for()
 
-#         # go to wrong fruit
-#         page.locator('button:has-text("wrong fruit")').click()
+        # go to wrong fruit
+        page_session.locator('button:has-text("wrong fruit")').click()
 
-#         # and follow the fallback link
-#         page.locator("text=Fruit not found, go to banana").click()
-#         page.locator("text=You chose banana").wait_for()
+        # and follow the fallback link
+        page_session.locator("text=Fruit not found, go to banana").click()
+        page_session.locator("text=You chose banana").wait_for()
 
-#         # another wrong link
-#         page.locator('button:has-text("wrong url")').click()
-#         page.locator("text=Page does not exist").wait_for()
+        # another wrong link
+        page_session.locator('button:has-text("wrong url")').click()
+        page_session.locator("text=Page does not exist").wait_for()
