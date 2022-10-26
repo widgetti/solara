376b114b7a3f93417dfff620a766592a8adcc6c9
Maarten A. Breddels <maartenbreddels@gmail.com>
2022-10-26 21:55:03 +0200

    more tests (error_test)

--- tests/integration/error_test.py
+++ tests/integration/error_test.py
@@ -1,13 +1,13 @@
-# from pathlib import Path
+from pathlib import Path
 
-# import playwright
-# import playwright.sync_api
+import playwright
+import playwright.sync_api
 
-# app_path = Path(__file__).parent / "testapp.py"
+app_path = Path(__file__).parent / "testapp.py"
 
 
-# def test_error_in_render(page: playwright.sync_api.Page, solara_server, solara_app, extra_include_path):
-#     with extra_include_path(app_path.parent), solara_app("testapp:clickboom"):
-#         page.goto(solara_server.base_url)
-#         page.locator("text=Boom").click()
-#         page.locator("text=I crash on 1").wait_for()
+def test_error_in_render(page_session: playwright.sync_api.Page, solara_server, solara_app, extra_include_path):
+    with extra_include_path(app_path.parent), solara_app("testapp:clickboom"):
+        page_session.goto(solara_server.base_url)
+        page_session.locator("text=Boom").click()
+        page_session.locator("text=I crash on 1").wait_for()
