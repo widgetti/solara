e71876e2886353c1fce386ad29538902b5033c7d
Maarten A. Breddels <maartenbreddels@gmail.com>
2022-11-04 11:35:31 +0100

    feat: support disabled and password type for InputText

--- solara/components/input.py
+++ solara/components/input.py
@@ -9,6 +9,8 @@ def InputText(
     label: str,
     value: str = "",
     on_value: Callable[[str], None] = None,
+    disabled: bool = False,
+    password: bool = False,
 ):
     """Free form text input.
 
@@ -17,7 +19,8 @@ def InputText(
     * `label`: Label to display next to the slider.
     * `value`: The currently entered value.
     * `on_value`: Callback to call when the value changes.
-    * `thumb_label`: Show a thumb label when sliding (True), always ("always"), or never (False).
+    * `disabled`: Whether the input is disabled.
+    * `password`: Whether the input is a password input (typically shows input text obscured with an asterisk).
     """
 
     def set_value_cast(value):
@@ -25,4 +28,4 @@ def InputText(
             return
         on_value(str(value))
 
-    return v.TextField(v_model=value, on_v_model=set_value_cast, label=label)
+    return v.TextField(v_model=value, on_v_model=set_value_cast, label=label, disabled=disabled, type="password" if password else None)
