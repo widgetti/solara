765d00dd85ed78c72a24e16e03451036cedf6795
Maarten A. Breddels <maartenbreddels@gmail.com>
2022-11-04 16:11:01 +0100

    feat: add continuous_update option to Inputs to only trigger on blur or enter
    This leads to less events being triggered, which is useful for performance reasons.

--- solara/components/input.py
+++ solara/components/input.py
@@ -1,7 +1,35 @@
-from typing import Callable
+from typing import Any, Callable, cast
+
+import ipyvue
+import reacton
 
 import solara
 from solara.alias import rv as v
+
+
+def use_change(el: reacton.core.Element, on_value: Callable[[Any], Any], enabled=True):
+    """Trigger a callback when a blur events occurs or the enter key is pressed."""
+    on_value_ref = solara.use_ref(on_value)
+    on_value_ref.current = on_value
+
+    def add_events():
+        def on_change(widget, event, data):
+            if enabled:
+                on_value_ref.current(widget.v_model)
+
+        widget = cast(ipyvue.VueWidget, solara.get_widget(el))
+        if enabled:
+            widget.on_event("blur", on_change)
+            widget.on_event("keyup.enter", on_change)
+
+        def cleanup():
+            if enabled:
+                widget.on_event("blur", on_change, remove=True)
+                widget.on_event("keyup.enter", on_change, remove=True)
+
+        return cleanup
+
+    solara.use_effect(add_events, [enabled])
 
 
 @solara.component
@@ -11,6 +39,7 @@ def InputText(
     on_value: Callable[[str], None] = None,
     disabled: bool = False,
     password: bool = False,
+    continuous_update: bool = False,
 ):
     """Free form text input.
 
@@ -21,6 +50,7 @@ def InputText(
     * `on_value`: Callback to call when the value changes.
     * `disabled`: Whether the input is disabled.
     * `password`: Whether the input is a password input (typically shows input text obscured with an asterisk).
+    * `continuous_update`: Whether to call the `on_value` callback on every change or only when the input loses focus or the enter key is pressed.
     """
 
     def set_value_cast(value):
@@ -28,7 +58,13 @@ def InputText(
             return
         on_value(str(value))
 
-    return v.TextField(v_model=value, on_v_model=set_value_cast, label=label, disabled=disabled, type="password" if password else None)
+    def on_v_model(value):
+        if continuous_update:
+            set_value_cast(value)
+
+    text_field = v.TextField(v_model=value, on_v_model=on_v_model, label=label, disabled=disabled, type="password" if password else None)
+    use_change(text_field, set_value_cast, enabled=not continuous_update)
+    return text_field
 
 
 @solara.component
@@ -37,6 +73,7 @@ def InputFloat(
     value: float = 0.0,
     on_value: Callable[[float], None] = None,
     disabled: bool = False,
+    continuous_update: bool = False,
 ):
     """Numeric input.
 
@@ -46,6 +83,7 @@ def InputFloat(
     * `value`: The currently entered value.
     * `on_value`: Callback to call when the value changes.
     * `disabled`: Whether the input is disabled.
+    * `continuous_update`: Whether to call the `on_value` callback on every change or only when the input loses focus or the enter key is pressed.
     """
 
     def set_value_cast(value):
@@ -58,4 +96,10 @@ def InputFloat(
             return
         on_value(float_value)
 
-    return v.TextField(v_model=value, on_v_model=set_value_cast, label=label, disabled=disabled, type="number")
+    def on_v_model(value):
+        if continuous_update:
+            set_value_cast(value)
+
+    text_field = v.TextField(v_model=value, on_v_model=on_v_model, label=label, disabled=disabled, type="number")
+    use_change(text_field, set_value_cast, enabled=not continuous_update)
+    return text_field
--- solara/website/pages/api/input.py
+++ solara/website/pages/api/input.py
@@ -14,23 +14,28 @@ def Page():
     password, set_password = solara.use_state("Super secret")
     number, set_number = solara.use_state(42.0)
 
+    continuous_update, set_continuous_update = solara.use_state(False)
+
     with solara.VBox() as main:
+        with solara.Card("Settings"):
+            solara.Checkbox(label="Continuous update", value=continuous_update, on_value=set_continuous_update)
+
         with solara.Card("Text"):
-            solara.InputText("Enter some text", value=text, on_value=set_text)
+            solara.InputText("Enter some text", value=text, on_value=set_text, continuous_update=continuous_update)
             with solara.HBox():
                 solara.Button("Clear", on_click=lambda: set_text(""))
                 solara.Button("Reset", on_click=lambda: set_text("Hello world"))
             solara.Markdown(f"**You entered**: {text}")
 
         with solara.Card("Password"):
-            solara.InputText("Enter a password", value=password, on_value=set_password, password=True)
+            solara.InputText("Enter a password", value=password, on_value=set_password, password=True, continuous_update=continuous_update)
             with solara.HBox():
                 solara.Button("Clear", on_click=lambda: set_password(""))
                 solara.Button("Reset", on_click=lambda: set_password("Super secret"))
             solara.Markdown(f"**You entered**: {password}")
 
         with solara.Card("Number (float)"):
-            solara.InputFloat("Enter some number", value=number, on_value=set_number)
+            solara.InputFloat("Enter some number", value=number, on_value=set_number, continuous_update=continuous_update)
             with solara.HBox():
                 solara.Button("Reset", on_click=lambda: set_number(42))
             solara.Markdown(f"**You entered**: {number}")
