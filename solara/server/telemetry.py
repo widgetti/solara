7f99b1e08be34fb161e2b13a35b4db3822a82447
Maarten A. Breddels <maartenbreddels@gmail.com>
2022-12-14 13:46:00 +0100

    feat: extend telemetry to include os, version and region

--- solara/server/telemetry.py
+++ solara/server/telemetry.py
@@ -1,5 +1,8 @@
 import json
 import logging
+import os
+import platform
+import threading
 import time
 import uuid
 from typing import Dict, Optional
@@ -18,7 +21,11 @@ logger = logging.getLogger("solara.server.telemetry")
 
 _server_user_id_override = None
 _server_start_time = time.time()
-
+# Privacy note: mixpanel does not store the IP, only the region
+_server_ip = None
+_platform_system = platform.system()
+_platform_release = platform.release()
+_python_version = platform.python_version()
 
 solara_props = {
     "solara_version": solara.__version__,
@@ -26,6 +33,25 @@ solara_props = {
     "ipyvuetify_version": ipyvuetify.__version__,
     "ipyvue_version": ipyvue.__version__,
 }
+_docker = False
+
+try:
+    path = "/proc/self/cgroup"
+    _docker = os.path.exists("/.dockerenv") or os.path.isfile(path) and any("docker" in line for line in open(path))
+except:  # noqa
+    logger.exception("Failed to detect docker")
+
+
+def _get_ip():
+    global _server_ip
+    try:
+        _server_ip = requests.get("https://api.ipify.org").text
+    except Exception:
+        logger.exception("Failed to get server IP")
+        _server_ip = "failed to get IP"
+
+
+threading.Thread(target=_get_ip())
 
 
 def override_server_user_id(server_user_id: str):
@@ -50,6 +76,12 @@ def track(event: str, props: Optional[Dict] = None):
             # can be useful to get of session duration
             "session_id": settings.telemetry.server_session_id,
             "$insert_id": str(uuid.uuid4()),  # to de-duplicate events
+            # Privacy note: mixpanel does not store the IP, only the region
+            "ip": _server_ip,
+            "platform_system": _platform_system,
+            "platform_release": _platform_release,
+            "python_version": _python_version,
+            "docker": _docker,
             **(solara_props or {}),
             **(props or {}),
         },
@@ -66,6 +98,14 @@ def track(event: str, props: Optional[Dict] = None):
 
 
 def server_start():
+    if _server_ip is None:
+        # wait at most 0.1 second to get the server ip
+        for i in range(10):
+            time.sleep(0.01)
+            if _server_ip is not None:
+                break
+        else:
+            logger.warning("Server IP is not known yet")
     global _server_start_time
     _server_start_time = time.time()
     track("Solara server start")
