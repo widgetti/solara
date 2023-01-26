e4c100d32b748f08350a9165fa5731d3d8569046
Maarten A. Breddels <maartenbreddels@gmail.com>
2023-01-26 16:16:31 +0100

    refactor: include title in html for ease of debugging

--- solara/components/title.vue
+++ solara/components/title.vue
@@ -1,9 +1,9 @@
-<template><span></span></template>
+<template><span style="display: none">{{ title }}</span></template>
 
 <script>
 module.exports = {
   mounted() {
-    if(window._solaraTitles === undefined) {
+    if (window._solaraTitles === undefined) {
       window._solaraTitles = [];
     }
     window._solaraTitles.push(this);
@@ -11,8 +11,8 @@ module.exports = {
   },
   destroyed() {
     window._solaraTitles.splice(window._solaraTitles.indexOf(this), 1);
-    if(window._solaraTitles.length) {
-        window._solaraTitles[0].updateTitle();
+    if (window._solaraTitles.length) {
+      window._solaraTitles[0].updateTitle();
     }
   },
   watch: {
@@ -26,8 +26,8 @@ module.exports = {
   methods: {
     updateTitle() {
       let deepestTitle = window._solaraTitles[0];
-      for(let i = 1; i < window._solaraTitles.length; i++) {
-        if(window._solaraTitles[i].level > deepestTitle.level) {
+      for (let i = 1; i < window._solaraTitles.length; i++) {
+        if (window._solaraTitles[i].level > deepestTitle.level) {
           deepestTitle = window._solaraTitles[i];
         }
       }
