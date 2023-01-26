4f159061fade8c8e27a2c2fd642b30b31a23ff4d
Maarten A. Breddels <maartenbreddels@gmail.com>
2023-01-26 16:13:40 +0100

    feat: make Column the default container

--- solara/components/__init__.py
+++ solara/components/__init__.py
@@ -40,3 +40,8 @@ from .echarts import FigureEcharts  # noqa: #F401 F403
 from .figure_altair import FigureAltair, AltairChart  # noqa: #F401 F403
 from .meta import Meta  # noqa: #F401 F403
 from .columns import Columns, ColumnsResponsive  # noqa: #F401 F403
+
+
+import reacton.core
+
+reacton.core._default_container = Column  # noqa: F405
