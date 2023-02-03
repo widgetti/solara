d707469d0c2f46d5d263efc96662f92a0c9ac833
Maarten A. Breddels <maartenbreddels@gmail.com>
2023-02-03 16:04:24 +0100

    feat+fix: improve image component by giving it width, css classes and a cursor fix
    Due to lumino, the cursor was set. This is now fixed in solara server.

--- solara/components/image.py
+++ solara/components/image.py
@@ -1,6 +1,6 @@
 import typing
 from pathlib import Path
-from typing import Union
+from typing import List, Optional, Union
 
 import solara
 from solara.alias import rw
@@ -10,13 +10,20 @@ if typing.TYPE_CHECKING:
 
 
 @solara.component
-def Image(image: Union[str, Path, "np.ndarray"]):
+def Image(image: Union[str, Path, "np.ndarray"], width: Optional[str] = None, classes: List[str] = []):
     """Displays an image from a URL, Path or numpy data.
 
     ## Arguments
      * `image`: URL, Path or numpy data.
-
+     * `width`: Width of the image, by default (None) the image is displayed at its original size.
+        Other options are: "100%" (to full its parent size), "100px" (to display it at 100 pixels wide),
+        or any other CSS width value.
+     * `classes`: List of CSS classes to add to the image.
     """
+    layout = {}
+    if width:
+        layout["width"] = width
+    classes = ["solara-image"] + classes
     if isinstance(image, (str, Path)):
         path = Path(image)
         if path.exists():
@@ -24,11 +31,26 @@ def Image(image: Union[str, Path, "np.ndarray"]):
 
             format = ipywidgets.Image._guess_format("image", str(path))
             value = ipywidgets.Image._load_file_value(str(path))
-            return rw.Image(value=value, format=format)
+            return rw.Image(
+                value=value,
+                format=format,
+                _dom_classes=classes,  # type: ignore
+                layout=layout,
+            )
         elif isinstance(image, str):
             url_data = image.encode("utf8")
-            return rw.Image(value=url_data, format="url")
+            return rw.Image(
+                value=url_data,
+                format="url",
+                _dom_classes=classes,  # type: ignore
+                layout=layout,
+            )
     elif solara.util.isinstanceof(image, "numpy:ndarray"):
         value = solara.util.numpy_to_image(image, format="png")
-        return rw.Image(value=value, format="image/png")
+        return rw.Image(
+            value=value,
+            format="image/png",
+            _dom_classes=classes,  # type: ignore
+            layout=layout,
+        )
     raise TypeError(f"Only support URL, path or numpy array, not {image}")
