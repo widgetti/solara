8c4c154be170844a48bc2584119bcabf7f7c75ed
Maarten A. Breddels <maartenbreddels@gmail.com>
2023-02-14 17:07:17 +0100

    tests: children text is offset due to added icon

--- tests/unit/file_download_test.py
+++ tests/unit/file_download_test.py
@@ -13,7 +13,7 @@ def test_download(tmpdir: Path):
     # lazily upload the data
     rc.find(FileDownloadWidget).widget.request_download = True
     rc.find(FileDownloadWidget, bytes=b"test content").wait_for()
-    assert rc.find(Btn).widget.children[0] == "Download: solara-download.dat"
+    assert rc.find(Btn).widget.children[1] == "Download: solara-download.dat"
 
     # change the data
     rc.render(solara.FileDownload(data="test content2"))
@@ -26,7 +26,7 @@ def test_download(tmpdir: Path):
     filename = tmpdir / "test.txt"
     filename.write_text("test content3", encoding="utf-8")
     rc.render(solara.FileDownload(data=filename.open("rb")))
-    assert rc.find(Btn).widget.children[0] == "Download: test.txt"
+    assert rc.find(Btn).widget.children[1] == "Download: test.txt"
     rc.find(FileDownloadWidget).widget.request_download = True
     rc.find(FileDownloadWidget, bytes=b"test content3").wait_for()
 
@@ -38,7 +38,7 @@ def test_download(tmpdir: Path):
         return b"lazy read"
 
     rc.render(solara.FileDownload(data=lazy_read))
-    assert rc.find(Btn).widget.children[0] == "Download: solara-download.dat"
+    assert rc.find(Btn).widget.children[1] == "Download: solara-download.dat"
     assert lazy_read_calls == 0
     rc.find(FileDownloadWidget).widget.request_download = True
     assert lazy_read_calls == 1
