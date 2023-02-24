036e60b64f16df2cf2f9eb4d72dd25aaef3f1c82
Maarten A. Breddels <maartenbreddels@gmail.com>
2023-02-24 14:10:54 +0100

    Bump version: 1.6.1 → 1.7.0

--- packages/assets/solara_assets/__init__.py
+++ packages/assets/solara_assets/__init__.py
@@ -1,2 +1,2 @@
 "CDN assets for Solara"
-__version__ = "1.6.1"
+__version__ = "1.7.0"
--- solara/__init__.py
+++ solara/__init__.py
@@ -1,5 +1,5 @@
 """Build webapps using IPywidgets"""
-__version__ = "1.6.1"
+__version__ = "1.7.0"
 github_url = "https://github.com/widgetti/solara"
 git_branch = "master"
 
