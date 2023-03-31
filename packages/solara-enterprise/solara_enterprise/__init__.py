be6f451e664c8bef17a394f5d620aa57b834a350
Maarten A. Breddels <maartenbreddels@gmail.com>
2023-03-31 13:01:32 +0200

    Bump version: 1.9.0 → 1.9.1

--- packages/solara-enterprise/solara_enterprise/__init__.py
+++ packages/solara-enterprise/solara_enterprise/__init__.py
@@ -1,2 +1,2 @@
 "Enterprise features for Solara"
-__version__ = "1.9.0"
+__version__ = "1.9.1"
--- solara/__init__.py
+++ solara/__init__.py
@@ -1,5 +1,5 @@
 """Build webapps using IPywidgets"""
-__version__ = "1.9.0"
+__version__ = "1.9.1"
 github_url = "https://github.com/widgetti/solara"
 git_branch = "master"
 
