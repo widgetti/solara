a20166394e78773cc3a6721a306a8f9221064dd3
Maarten A. Breddels <maartenbreddels@gmail.com>
2023-05-05 19:05:10 +0200

    feat: support PIL and bytes for solara.Image

--- solara/components/image.py
+++ solara/components/image.py
@@ -1,4 +1,5 @@
 import typing
+from io import BytesIO
 from pathlib import Path
 from typing import List, Optional, Union
 
@@ -7,17 +8,116 @@ from solara.alias import rw
 
 if typing.TYPE_CHECKING:
     import numpy as np
+    import PIL.Image
 
 
 @solara.component
-def Image(image: Union[str, Path, "np.ndarray"], width: Optional[str] = None, classes: List[str] = []):
-    """Displays an image from a URL, Path or numpy data.
+def Image(
+    image: Union[str, Path, "np.ndarray", bytes, "PIL.Image.Image"],
+    width: Optional[str] = None,
+    format="png",
+    classes: List[str] = [],
+):
+    """Displays an image from a URL, Path, numpy data, bytes or PIL image.
+
+    If passed as bytes, the image is assumed to be in the format specified by the `format` argument.
+
+    ## Examples
+
+    ### Displaying an image from a Path
+
+    ```solara
+    import solara
+    import solara.website
+    from pathlib import Path
+
+
+    image_path = Path(solara.website.__file__).parent / "public/beach.jpeg"
+
+    @solara.component
+    def Page():
+        solara.Image(image_path)
+    ```
+
+    ### Displaying an image from a URL
+
+    ```solara
+    import solara
+
+
+    image_url = "/static/public/beach.jpeg"
+
+    @solara.component
+    def Page():
+        solara.Image(image_url)
+        display(image_url)
+
+    ```
+
+    ### Displaying an image from a numpy array
+
+    ```solara
+
+    import solara
+    import solara.website
+    from pathlib import Path
+    import numpy as np
+    import PIL.Image
+
+
+    image_path = Path(solara.website.__file__).parent / "public/beach.jpeg"
+    image_ndarray = np.asarray(PIL.Image.open(image_path))
+
+    @solara.component
+    def Page():
+        solara.Image(image_ndarray)
+        display(image_ndarray)
+    ```
+
+    ### Displaying an image from bytes
+
+    ```solara
+
+    import solara
+    import solara.website
+    from pathlib import Path
+    import PIL.Image
+
+
+    image_path = Path(solara.website.__file__).parent / "public/beach.jpeg"
+    image_bytes = image_path.read_bytes()
+
+    @solara.component
+    def Page():
+        solara.Image(image_bytes, format="jpeg")
+        display(image_bytes[:100])
+    ```
+
+    ### Displaying an image from a PIL Image
+
+    ```solara
+
+    import solara
+    import solara.website
+    from pathlib import Path
+    import PIL.Image
+
+
+    image_path = Path(solara.website.__file__).parent / "public/beach.jpeg"
+    image = PIL.Image.open(image_path)
+
+    @solara.component
+    def Page():
+        solara.Image(image)
+    ```
+
 
     ## Arguments
-     * `image`: URL, Path or numpy data.
+     * `image`: URL, Path, numpy data, bytes or PIL Image.
      * `width`: Width of the image, by default (None) the image is displayed at its original size.
         Other options are: "100%" (to full its parent size), "100px" (to display it at 100 pixels wide),
         or any other CSS width value.
+     * `format`: Format of the image, only used when image is of type bytes, or a PIL Image is passed.
      * `classes`: List of CSS classes to add to the image.
     """
     layout = {}
@@ -45,11 +145,28 @@ def Image(image: Union[str, Path, "np.ndarray"], width: Optional[str] = None, cl
                 _dom_classes=classes,  # type: ignore
                 layout=layout,
             )
+    elif isinstance(image, bytes):
+        return rw.Image(
+            value=image,
+            format=format,
+            _dom_classes=classes,  # type: ignore
+            layout=layout,
+        )
+    elif solara.util.isinstanceof(image, "PIL.Image:Image"):
+        f = BytesIO()
+        image.save(f, format=format)  # type: ignore
+        value = f.getvalue()
+        return rw.Image(
+            value=value,
+            format="png",
+            _dom_classes=classes,  # type: ignore
+            layout=layout,
+        )
     elif solara.util.isinstanceof(image, "numpy:ndarray"):
         value = solara.util.numpy_to_image(image, format="png")
         return rw.Image(
             value=value,
-            format="image/png",
+            format="png",
             _dom_classes=classes,  # type: ignore
             layout=layout,
         )
--- solara/website/pages/api/image.py
+++ solara/website/pages/api/image.py
@@ -1,34 +1,13 @@
 """# Image
+
 """
-
-from pathlib import Path
-
-import numpy as np
-import PIL.Image
-
 import solara
-import solara.website
 from solara.website.utils import apidoc
 
+from . import NoPage
 
-@solara.component
-def Page():
-
-    image_path = Path(solara.website.__file__).parent / "public/beach.jpeg"
-    image_url = "/static/public/beach.jpeg"
-    image_ndarray = np.asarray(PIL.Image.open(image_path))
-
-    with solara.VBox() as main:
-        with solara.Card(title="As a path"):
-            solara.Image(image_path)
-
-        with solara.Card(title="As a URL"):
-            solara.Image(image_url)
-
-        with solara.Card(title="As NumPy array"):
-            solara.Image(image_ndarray)
-
-    return main
+Page = NoPage
+title = "Image"
 
 
 __doc__ += apidoc(solara.Image.f)  # type: ignore
