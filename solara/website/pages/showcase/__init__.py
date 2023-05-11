e2e570a07febd638fedc232fa7894ade08e14691
Maarten A. Breddels <maartenbreddels@gmail.com>
2023-05-11 22:48:44 +0200

    docs: add bulk label to showcase

--- solara/website/pages/showcase/__init__.py
+++ solara/website/pages/showcase/__init__.py
@@ -33,3 +33,22 @@ def Page():
             )
             with solara.Link("./planeto_tessa"):
                 solara.Image("/static/public/showcase/tessa/thumbnail.png", width="100%")
+
+        with solara.Card("Bulk labeling", style={"height": "100%"}):
+            solara.Markdown(
+                """
+            [Bulk labeling](https://github.com/Ben-Epstein/solara-examples/tree/main/bulk-labeling)
+            is a tool developed by Ben Epstein for labeling data in bulk."""
+            )
+            from solara.alias import rv
+
+            img = "https://avatars.githubusercontent.com/u/22605641?v=4"
+            with rv.Html(tag="a", attributes={"href": "https://github.com/Ben-Epstein/solara-examples/tree/main/bulk-labeling", "target": "_blank"}):
+                solara.Image("https://user-images.githubusercontent.com/1765949/237517090-8f7242b1-3189-4c5b-abd3-0f0986292ade.png", width="100%")
+            with rv.Html(tag="a", attributes={"href": "https://github.com/Ben-Epstein/", "target": "_blank"}):
+                with rv.ListItem(class_="grow"):
+                    with rv.ListItemAvatar(color="grey darken-3"):
+                        rv.Img(
+                            class_="elevation-6",
+                            src=img,
+                        )
