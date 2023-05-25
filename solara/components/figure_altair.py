cada5c7c8a7cc17351b33df08f67393192159a21
Maarten A. Breddels <maartenbreddels@gmail.com>
2023-05-25 15:29:21 +0200

    fix: support altair v5

--- solara/components/figure_altair.py
+++ solara/components/figure_altair.py
@@ -23,10 +23,11 @@ def FigureAltair(
 
     with alt.renderers.enable("mimetype"):
         bundle = chart._repr_mimebundle_()[0]
-        key = "application/vnd.vegalite.v4+json"
-        if key not in bundle:
-            raise KeyError(f"{key} not in mimebundle:\n\n{bundle}")
-        spec = bundle[key]
+        key4 = "application/vnd.vegalite.v4+json"
+        key5 = "application/vnd.vegalite.v5+json"
+        if key4 not in bundle and key5 not in bundle:
+            raise KeyError(f"{key4} and {key5} not in mimebundle:\n\n{bundle}")
+        spec = bundle.get(key5, bundle.get(key4))
         return solara.widgets.VegaLite.element(
             spec=spec, on_click=on_click, listen_to_click=on_click is not None, on_hover=on_hover, listen_to_hover=on_hover is not None
         )
