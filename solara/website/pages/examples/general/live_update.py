f9b1d11c55f9092cfff71581e4850fb9f6e164af
alisterburt <alisterburt@gmail.com>
2023-08-02 10:28:46 +0100

    add explanatory docs

--- solara/website/pages/examples/general/live_update.py
+++ solara/website/pages/examples/general/live_update.py
@@ -10,22 +10,28 @@ plt.switch_backend('agg')
 
 @solara.component
 def Page():
+    # define some state which will be updated regularly in a separate thread
     counter = solara.use_reactive(0)
 
     def render():
+        """Infinite loop regularly mutating counter state"""
         while True:
             sleep(0.2)
             counter.value += 1
 
+    # run the render loop in a separate thread
     result = solara.use_thread(render)
     if result.error:
         raise result.error
-    # force the DataDashboard to redraw, by making it depends on the counter value
-    DataDashboard(counter.value)
+
+    # create the LiveUpdatingComponent, this component depends on the counter
+    # value so will be redrawn whenever counter value changes
+    LiveUpdatingComponent(counter.value)
 
 
 @solara.component
-def DataDashboard(counter):
+def LiveUpdatingComponent(counter):
+    """Component which will be redrawn whenever the counter value changes."""
     fig, ax = plt.subplots()
     ax.plot(np.arange(10), np.random.random(10))
     solara.FigureMatplotlib(fig)
