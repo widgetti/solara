6447ff463404c106b0995b0a3f74867bd37deccc
Hans Nowak <hansnowak@widgetti.io>
2023-09-14 15:59:21 +0200

    added another test (currently failing)

--- tests/unit/confirmation_dialog_test.py
+++ tests/unit/confirmation_dialog_test.py
@@ -16,3 +16,17 @@ def test_confirmation_dialog():
     buttons[0].widget.click()
     assert on_ok.call_count == 1  # was OK button clicked?
     assert not is_open.value  # is dialog closed?
+
+
+def test_confirmation_dialog_custom_button():
+    is_open = solara.reactive(True)
+    on_ok = MagicMock()
+    my_button = solara.Button(label="Not OK")  # on_click?
+    el = ConfirmationDialog(is_open, on_ok=on_ok, ok=my_button)
+    _, rc = solara.render(el, handle_error=False)
+    buttons = rc.find(vw.Btn)
+    assert len(buttons) == 2
+    assert buttons[0].widget.children == ["Not OK"]
+    assert buttons[1].widget.children == ["Cancel"]
+    buttons[0].widget.click()
+    assert on_ok.call_count == 1  # will fail
