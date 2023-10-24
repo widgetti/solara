baf4eae557cd0d333df366842ebc1cce92307957
Maarten A. Breddels <maartenbreddels@gmail.com>
2023-10-24 11:19:56 +0200

    format and put token into a separate component

--- solara/website/pages/examples/ai/tokenizer.py
+++ solara/website/pages/examples/ai/tokenizer.py
@@ -10,11 +10,13 @@ Inspired by the Understanding GPT tokenizers by Simon Willison
 Install pandas and tiktoken with `pip install pandas tiktoken`
 
 """
-import pandas as pd
-import tiktoken
 import random
 
+import pandas as pd
+import tiktoken
+
 import solara
+import solara.lab
 
 # Get tokenizer for gpt-4
 tokenizer = tiktoken.encoding_for_model("gpt-4")
@@ -27,45 +29,57 @@ df["token"] = [tokenizer.decode([i]) for i in range(50257)]
 text1 = solara.reactive("Example text is here")
 text2 = solara.reactive("")
 text3 = solara.reactive("")
+
+
+@solara.component
+def Token(token: int):
+    random.seed(token)
+    random_color = "".join([random.choice("0123456789ABCDEF") for k in range(6)])
+    with solara.Div(style="display: inline;"):
+        with solara.Div(
+            style={
+                "display": "inline",
+                "padding": "6px",
+                "border-right": "3px solid white",
+                "line-height": "3em",
+                "font-family": "courier",
+                "background-color": f"#{random_color}",
+                "color": "white",
+                "position": "relative",
+            },
+        ):
+            solara.Text(
+                " " + str(token),
+                style={
+                    "display": "inline",
+                    "position": "absolute",
+                    "top": "5.5ch",
+                    "line-height": "1em",
+                    "left": "-0.5px",
+                    "font-size": "0.45em",
+                },
+            )
+            solara.Text(str(tokenizer.decode([token])))
+
+
 @solara.component
 def Page():
-  with solara.Column(margin=10) as main:
-    solara.Markdown("#GPT-4 token encoder and decoder")
-    solara.Markdown("This is an educational tool for understanding how tokenization works.")
-    solara.InputText("Enter text to tokenize it:", value=text1, continuous_update=True)
-    tokens = tokenizer.encode(text1.value)
-    spans1 = ""
-    spans2 = ""
-    for i, token in enumerate(tokens):
-  	# create random color dependent on the position
-      random.seed(i)
-      random_color = ''.join([random.choice('0123456789ABCDEF') for k in range(6)])
-      spans1 += " " + f"<span style='font-family: helvetica; color: #{random_color}'>{token}</span>"
-      spans2 += " " + f"""<span style="
-          padding: 6px;
-          border-right: 3px solid white;
-          line-height: 3em;
-          font-family: courier;
-          background-color: #{random_color};
-          color: white;
-          position: relative;
-        "><span style="
-        position: absolute;
-        top: 5.5ch;
-        line-height: 1em;
-        left: -0.5px;
-        font-size: 0.45em"> {token}</span>{tokenizer.decode([token])}</span>"""
-    solara.Markdown(f"{spans1}")
-    solara.Markdown(f"{len(tokens)} token") if len(tokens)==1 else solara.Markdown(f"{len(tokens)} tokens")
-    solara.Markdown(f'{spans2}')
-    solara.InputText("Or convert space separated tokens to text:", value=text2, continuous_update=True)
-    token_input = text2.value.split(' ')
-    token_input = [int(span) for span in token_input if span != ""]
-    text_output = tokenizer.decode(token_input)
-    solara.Markdown(f'{text_output}')
-    solara.Markdown("##Search tokens")
-    solara.InputText("Search for a token:", value=text3, continuous_update=True)
-    df_subset = df[df["token"].str.startswith(text3.value)]
-    solara.Markdown(f"{df_subset.shape[0]:,} results")
-    solara.DataFrame(df_subset, items_per_page=10)
-  return main
+    with solara.Column(margin=10) as main:
+        solara.Markdown("#GPT-4 token encoder and decoder")
+        solara.Markdown("This is an educational tool for understanding how tokenization works.")
+        solara.InputText("Enter text to tokenize it:", value=text1, continuous_update=True)
+        tokens = tokenizer.encode(text1.value)
+        with solara.Div(style="display: inline;"):
+            for i, token in enumerate(tokens):
+                Token(token)
+        # create random color dependent on the position
+        solara.InputText("Or convert space separated tokens to text:", value=text2, continuous_update=True)
+        token_input = [int(span) for span in text2.value.split(" ") if span != ""]
+        text_output = tokenizer.decode(token_input)
+        solara.Markdown(f"{text_output}")
+        solara.Markdown("##Search tokens")
+        solara.InputText("Search for a token:", value=text3, continuous_update=True)
+        df_subset = df[df["token"].str.startswith(text3.value)]
+        solara.Markdown(f"{df_subset.shape[0]:,} results")
+        solara.DataFrame(df_subset, items_per_page=10)
+    return main
