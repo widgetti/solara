5afbc8ee362aef0e693d237044fc0612586bb9e9
Jochem Smit <jhsmit@gmail.com>
2023-11-10 14:45:05 +0100

    test: add polars DataTable test

--- tests/unit/datatable_test.py
+++ tests/unit/datatable_test.py
@@ -1,13 +1,14 @@
 import pytest
 import solara
 import vaex
+import polars as pl
 from solara.components.datatable import DataTable, DataTableWidget
 
 df_vaex = vaex.datasets.titanic()
 df_pandas = df_vaex.to_pandas_df()
+df_polars = pl.from_pandas(df_pandas)
 
-
-@pytest.mark.parametrize("df", [df_vaex, df_pandas])
+@pytest.mark.parametrize("df", [df_vaex, df_pandas, df_polars])
 def test_render(df):
     @solara.component
     def Test():
