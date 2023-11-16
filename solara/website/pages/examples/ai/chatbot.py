289ac5c2da4479a3ea8844862fab0b2807ebca05
Iisakki Rotko <iisakki.rotko@gmail.com>
2023-11-16 17:19:48 +0100

    feat: Chat components, and AI example of use

--- solara/website/pages/examples/ai/chatbot.py
+++ solara/website/pages/examples/ai/chatbot.py
@@ -23,7 +23,7 @@ messages: solara.Reactive[List[Dict[str, Union[str, bool]]]] = solara.reactive([
 
 @solara.component
 def Page():
-    user_message_count = len(m for messages.value if m.user)
+    user_message_count = len(m for m in messages.value if m.user)
 
     def send(message):
         messages.set(
