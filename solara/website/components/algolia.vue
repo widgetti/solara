fda0c08f9df2c3fb9be532dd1d52d682bfe5f7d6
Maarten Breddels <maartenbreddels@gmail.com>
2023-11-22 20:23:39 +0100

    docs: have search/algolia work before hydration (#394)
    We run the initialize of algolia asap now, so it's functional before
    hydration.
    The problem was that the JS part of the vue template was not executed
    before hydration, so the algolia object was not initialized.

--- solara/website/components/algolia.vue
+++ solara/website/components/algolia.vue
@@ -7,44 +7,7 @@
 module.exports = {
     mounted() {
         // like open in new window
-        const otherClick = (event) => event.button === 1 || event.altKey || event.ctrlKey || event.metaKey || event.shiftKey
-        docsearch({
-            container: '#docsearch',
-            appId: this.app_id,
-            apiKey: this.api_key,
-            indexName: this.index_name,
-            debug: this.debug,
-            hitComponent: ({ hit, children }) => {
-                // react without the React library
-                return {
-                    type: 'a',
-                    constructor: undefined,
-                    __v: 1,
-                    props: {
-                        href: hit.url,
-                        children,
-                        onClick: (event) => {
-                            if (otherClick(event)) { return }
-                            event.preventDefault()
-                            let url = hit.url;
-                            if (url.startsWith('https://solara.dev')) {
-                                url = url.substring(18);
-                            }
-                            solara.router.push(url)
-                        }
-                    }
-                }
-            },
-
-            navigator: {
-                navigate({ itemUrl }) {
-                    if (itemUrl.startsWith('https://solara.dev')) {
-                        itemUrl = itemUrl.substring(18);
-                    }
-                    solara.router.push(itemUrl);
-                },
-            },
-        });
+        solaraAlgoliaInit({appId: this.app_id, apiKey: this.api_key, indexName: this.index_name, debug: this.debug})
     }
 }
 </script>
