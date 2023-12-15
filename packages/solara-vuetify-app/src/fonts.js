d145b520928f68e7ca90021f0d92c450d2f3e9cc
Maarten A. Breddels <maartenbreddels@gmail.com>
2023-12-15 11:20:39 +0100

    performance: load roboto and mdi css after first paint

---
+++ packages/solara-vuetify-app/src/fonts.js
@@ -0,0 +1,3 @@
+import 'typeface-roboto';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import '@mdi/font/css/materialdesignicons.css';
--- packages/solara-vuetify3-app/src/solara-vuetify-app.js
+++ packages/solara-vuetify3-app/src/solara-vuetify-app.js
@@ -1,7 +1,3 @@
-import '@mdi/font/css/materialdesignicons.css';
-import 'material-design-icons-iconfont/dist/material-design-icons.css';
-import 'typeface-roboto';
-
 import * as Vue from 'vue';
 import * as Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
