09b888e55e1ead034d5e42dfaf563ffbe5c74508
Mario Buikhuizen <mariobuikhuizen@widgetti.io>
2024-01-11 17:50:56 +0100

    fix: use smaller webp format for images in tutorial 1
    And use our CDN to host them.

--- solara/website/pages/docs/content/04-tutorial/60-jupyter-dashboard-part1.py
+++ solara/website/pages/docs/content/04-tutorial/60-jupyter-dashboard-part1.py
@@ -38,14 +38,19 @@ def Page():
         execute=False,
         outputs={
             "a7d17a84": None,  # empty output (7)
-            "82f1d2f7": solara.Image("https://github.com/widgetti/solara/assets/1765949/e844acdb-c77d-4df4-ba4c-a629f92f18a3"),  # map (11)
+            # original: https://github.com/widgetti/solara/assets/1765949/e844acdb-c77d-4df4-ba4c-a629f92f18a3
+            "82f1d2f7": solara.Image("https://dxhl76zpt6fap.cloudfront.net/pages/docs/content/60-jupyter-dashboard-part1/map.webp"),  # map (11)
             "3e7ea361": None,  # (13)
-            "56055643": solara.Image("https://github.com/widgetti/solara/assets/1765949/daaa3a46-61f5-431f-8003-b42b5915da4b"),  # View (15)
-            "c78010ec": solara.Image("https://github.com/widgetti/solara/assets/1765949/2f4daf0f-b7d8-4f70-b04a-c27542cffdb0"),  # Page (20)
-            "18290364": solara.Image("https://github.com/widgetti/solara/assets/1765949/a691d9f1-f07b-4e06-b21b-20980476ad64"),  # Controls
+            # original: https://github.com/widgetti/solara/assets/1765949/daaa3a46-61f5-431f-8003-b42b5915da4b
+            "56055643": solara.Image("https://dxhl76zpt6fap.cloudfront.net/pages/docs/content/60-jupyter-dashboard-part1/view.webp"),  # View (15)
+            # original: https://github.com/widgetti/solara/assets/1765949/2f4daf0f-b7d8-4f70-b04a-c27542cffdb0
+            "c78010ec": solara.Image("https://dxhl76zpt6fap.cloudfront.net/pages/docs/content/60-jupyter-dashboard-part1/page.webp"),  # Page (20)
+            # original: https://github.com/widgetti/solara/assets/1765949/a691d9f1-f07b-4e06-b21b-20980476ad64
+            "18290364": solara.Image("https://dxhl76zpt6fap.cloudfront.net/pages/docs/content/60-jupyter-dashboard-part1/controls.webp"),  # Controls
             "0ca68fe8": None,
             "fef5d187": None,
-            "af686391": solara.Image("https://github.com/widgetti/solara/assets/1765949/f0075ad1-808d-458c-8797-e460ce4dc06d"),  # Full app
+            # original: https://github.com/widgetti/solara/assets/1765949/f0075ad1-808d-458c-8797-e460ce4dc06d
+            "af686391": solara.Image("https://dxhl76zpt6fap.cloudfront.net/pages/docs/content/60-jupyter-dashboard-part1/full-app.webp"),  # Full app
         },
     )
     solara.Markdown(
