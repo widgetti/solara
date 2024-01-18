d199572007515bd22361a18d6ddc723a8a0f4611
Maarten Breddels <maartenbreddels@gmail.com>
2024-01-18 14:28:33 +0100

    Update solara/website/pages/api/task.py
    Co-authored-by: Iisakki Rotko <iisakki.rotko@widgetti.io>

--- solara/website/pages/api/task.py
+++ solara/website/pages/api/task.py
@@ -1,17 +1,6 @@
-"""# task
+"""# Task
 
-Decorator to turn a function or coroutine function into a task.
-A task is a callable that will run the function in a separate thread for normal functions
-or a asyncio task for a coroutine function.
-
-The task callable does will only execute the function once when called multiple times,
-and will cancel previous executions if the function is called again before the previous finished.
-
-
-The wrapped function return value is available as the `.value` attribute of the task object.
-
-
-
+A global way to run code in the background, with the UI available to the user. This is useful for long running tasks, like downloading data.
 
 """
 import solara
