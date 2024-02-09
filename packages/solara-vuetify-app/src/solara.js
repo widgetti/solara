70b8e0ebab06745cf8239f41bd25218ccb1281dd
Iisakki Rotko <iisakki.rotko@widgetti.io>
2024-02-09 14:00:41 +0100

    Remove last reference to `renderMermaid`

--- packages/solara-vuetify-app/src/solara.js
+++ packages/solara-vuetify-app/src/solara.js
@@ -1,1 +1,1 @@
-export { RenderMimeRegistry, WidgetManager, connectKernel, extendedRendererFactories, renderMathJax, renderMermaid, shutdownKernel } from '@widgetti/solara-widget-manager';
+export { RenderMimeRegistry, WidgetManager, connectKernel, extendedRendererFactories, renderMathJax, shutdownKernel } from '@widgetti/solara-widget-manager';
