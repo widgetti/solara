150a4652a081814df33cca383c3ef1b5beaa3038
Maarten A. Breddels <maartenbreddels@gmail.com>
2024-02-23 16:05:42 +0100

    Bump version solara-vuetify3-app: 3.0.0 → 4.0.0

--- packages/assets/hatch_build.py
+++ packages/assets/hatch_build.py
@@ -7,7 +7,7 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 packages = [
     ["@widgetti/solara-vuetify-app", "9.0.0"],
-    ["@widgetti/solara-vuetify3-app", "3.0.0"],
+    ["@widgetti/solara-vuetify3-app", "4.0.0"],
     ["requirejs", "2.3.6"],
     ["mermaid", "10.8.0"],
     ["codemirror", "5.65.3"],
