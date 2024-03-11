78995d445ccca75a76aa76587f9c98d3a8c1c08b
Iisakki Rotko <iisakki.rotko@gmail.com>
2024-03-11 15:36:03 +0100

    Bump version solara-vuetify3-app: 5.0.0 → 5.0.1

--- release.sh
+++ release.sh
@@ -5,16 +5,16 @@ set -e -o pipefail
     echo -e "\033[31m There are unreleased changes to the solara-vuetify-app package.\n Please release the javascript package before Solara by running \n\n \
     \033[0m (cd packages/solara-vuetify-app && ./release.sh <patch | minor | major> -n)\n"; \
     exit 1
-(git diff --quiet master @widgetti/solara-vuetify3-app@5.0.0 -- packages/solara-vuetify3-app) || \
+(git diff --quiet master @widgetti/solara-vuetify3-app@5.0.1 -- packages/solara-vuetify3-app) || \
     echo -e "\033[31m There are unreleased changes to the solara-vuetify3-app package.\n Please release the javascript package before Solara by running \n\n \
     \033[0m (cd packages/solara-vuetify3-app && ./release.sh <patch | minor | major> -n)\n"; \
     exit 1
-(git diff --quiet master @widgetti/solara-vuetify3-app@5.0.0 -- packages/solara-widget-manager) || \
+(git diff --quiet master @widgetti/solara-vuetify3-app@5.0.1 -- packages/solara-widget-manager) || \
     echo -e "\033[31m There are unreleased changes to the solara-widget-manager package.\n Please release the javascript package before Solara by running \n\n \
     \033[0m (cd packages/solara-vuetify-app && ./release.sh <patch | minor | major> -n) && \
     (cd packages/solara-vuetify3-app && ./release.sh <patch | minor | major> -n)\n"; \
     exit 1
-(git diff --quiet master @widgetti/solara-vuetify3-app@5.0.0 -- packages/solara-widget-manager8) || \
+(git diff --quiet master @widgetti/solara-vuetify3-app@5.0.1 -- packages/solara-widget-manager8) || \
     echo -e "\033[31m There are unreleased changes to the solara-widget-manager8 package.\n Please release the javascript package before Solara by running \n\n \
     \033[0m (cd packages/solara-vuetify-app && ./release.sh <patch | minor | major> -n) && \
     (cd packages/solara-vuetify3-app && ./release.sh <patch | minor | major> -n)\n"; \
