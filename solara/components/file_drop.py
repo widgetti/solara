e77139ae6901ccd847858aa7b919081df3af488b
Huseyin Kaya <hkayabilisim@gmail.com>
2024-03-16 21:26:24 +0300

    feat: multiple file support for file_drop
    Co-authored-by: Iisakki Rotko <iisakki.rotko@gmail.com>

--- solara/components/file_drop.py
+++ solara/components/file_drop.py
@@ -1,6 +1,6 @@
 import threading
 import typing
-from typing import Callable, Optional, cast
+from typing import Callable, List, Optional, Union, cast
 
 import traitlets
 from ipyvue import Template
@@ -25,45 +25,26 @@ class FileDropZone(FileInput):
     template_file = (__file__, "file_drop.vue")
     items = traitlets.List(default_value=[]).tag(sync=True)
     label = traitlets.Unicode().tag(sync=True)
+    multiple = traitlets.Bool(True).tag(sync=True)
 
 
 @solara.component
-def FileDrop(
-    label="Drop file here",
+def _FileDrop(
+    label="Drop file(s) here",
     on_total_progress: Optional[Callable[[float], None]] = None,
-    on_file: Optional[Callable[[FileInfo], None]] = None,
+    on_file: Optional[Callable[[Union[FileInfo, List[FileInfo]]], None]] = None,
     lazy: bool = True,
+    multiple: bool = False,
 ):
-    """Region a user can drop a file into for file uploading.
+    """Generic implementation used by FileDrop and FileDropMultiple.
 
-    If lazy=True, no file content will be loaded into memory,
-    nor will any data be transferred by default.
-    A file object is passed to the `on_file` callback, and data will be transferred
-    when needed.
-
-    If lazy=False, the file content will be loaded into memory and passed to the `on_file` callback via the `.data` attribute.
-
-    The on_file callback takes the following argument type:
-    ```python
-    class FileInfo(typing.TypedDict):
-        name: str  # file name
-        size: int  # file size in bytes
-        file_obj: typing.BinaryIO
-        data: Optional[bytes]: bytes  # only present if lazy=False
-    ```
-
-
-    ## Arguments
-     * `on_total_progress`: Will be called with the progress in % of the file upload.
-     * `on_file`: Will be called with a `FileInfo` object, which contains the file `.name`, `.length` and a `.file_obj` object.
-     * `lazy`: Whether to load the file content into memory or not. If `False`,
-        the file content will be loaded into memory and passed to the `on_file` callback via the `.data` attribute.
-
+    If multiple=True, multiple files can be uploaded.
     """
+
     file_info, set_file_info = solara.use_state(None)
     wired_files, set_wired_files = solara.use_state(cast(Optional[typing.List[FileInfo]], None))
 
-    file_drop = FileDropZone.element(label=label, on_total_progress=on_total_progress, on_file_info=set_file_info)  # type: ignore
+    file_drop = FileDropZone.element(label=label, on_total_progress=on_total_progress, on_file_info=set_file_info, multiple=multiple)  # type: ignore
 
     def wire_files():
         if not file_info:
@@ -83,14 +64,76 @@ def FileDrop(
         if not wired_files:
             return
         if on_file:
-            if not lazy:
-                wired_files[0]["data"] = wired_files[0]["file_obj"].read()
+            for i in range(len(wired_files)):
+                if not lazy:
+                    wired_files[i]["data"] = wired_files[i]["file_obj"].read()
+                else:
+                    wired_files[i]["data"] = None
+            if multiple:
+                on_file(wired_files)
             else:
-                wired_files[0]["data"] = None
-            on_file(wired_files[0])
+                on_file(wired_files[0])
 
     result: solara.Result = hooks.use_thread(handle_file, [wired_files])
     if result.error:
         raise result.error
 
     return file_drop
+
+
+@solara.component
+def FileDrop(
+    label="Drop file here",
+    on_total_progress: Optional[Callable[[float], None]] = None,
+    on_file: Optional[Callable[[FileInfo], None]] = None,
+    lazy: bool = True,
+):
+    """Region a user can drop a file into for file uploading.
+
+    If lazy=True, no file content will be loaded into memory,
+    nor will any data be transferred by default.
+    If lazy=False, file content will be loaded into memory and passed to the `on_file` callback via the `FileInfo.data` attribute.
+
+
+    A file object is of the following argument type:
+    ```python
+    class FileInfo(typing.TypedDict):
+        name: str  # file name
+        size: int  # file size in bytes
+        file_obj: typing.BinaryIO
+        data: Optional[bytes]: bytes  # only present if lazy=False
+    ```
+
+
+    ## Arguments
+     * `on_total_progress`: Will be called with the progress in % of the file upload.
+     * `on_file`: Will be called with a `FileInfo` object, which contains the file `.name`, `.length` and a `.file_obj` object.
+     * `lazy`: Whether to load the file contents into memory or not. If `False`,
+        the file contents will be loaded into memory via the `.data` attribute of file object(s).
+
+    """
+
+    return _FileDrop(label=label, on_total_progress=on_total_progress, on_file=on_file, lazy=lazy, multiple=False)
+
+
+@solara.component
+def FileDropMultiple(
+    label="Drop files here",
+    on_total_progress: Optional[Callable[[float], None]] = None,
+    on_file: Optional[Callable[[List[FileInfo]], None]] = None,
+    lazy: bool = True,
+):
+    """Region a user can drop multiple files into for file uploading.
+
+    Almost identical to `FileDrop` except that multiple files can be dropped and `on_file` is called
+    with a list of `FileInfo` objects.
+
+    ## Arguments
+     * `on_total_progress`: Will be called with the progress in % of the file(s) upload.
+     * `on_file`: Will be called with a `List[FileInfo]`.
+        Each `FileInfo` contains the file `.name`, `.length`, `.file_obj` object, and `.data` attributes.
+     * `lazy`: Whether to load the file contents into memory or not.
+
+    """
+
+    return _FileDrop(label=label, on_total_progress=on_total_progress, on_file=on_file, lazy=lazy, multiple=True)
