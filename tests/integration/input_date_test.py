702a2d6264511d59a2bac200842bc85d0678c0ae
Iisakki Rotko <iisakki.rotko@widgetti.io>
2024-05-31 11:07:55 +0200

    fix(ci): test_input_date_range failing on last day of the month (#662)
    Instead of running the tests on today and tomorrow, let's fix them to a particular date.

--- tests/integration/input_date_test.py
+++ tests/integration/input_date_test.py
@@ -4,8 +4,8 @@ from unittest.mock import MagicMock
 
 from playwright.sync_api import Page, expect
 
-today = dt.date.today()
-tomorrow = today + dt.timedelta(days=1)
+date = dt.date(2018, 9, 1)
+date2 = date + dt.timedelta(days=1)
 
 
 def test_input_date_single(solara_test, page_session: Page):
@@ -20,24 +20,24 @@ def test_input_date_single(solara_test, page_session: Page):
             on_value(value)
             set_value(value)
 
-        value, set_value = solara.use_state(today)
+        value, set_value = solara.use_state(date)
         InputDate(value=value, label="label", on_value=update_value, classes=["test-class"])
 
     solara.display(Page())
 
     input = page_session.locator(".test-class input")
     expect(input).to_be_visible()
-    expect(input).to_have_value(today.strftime("%Y/%m/%d"))
+    expect(input).to_have_value(date.strftime("%Y/%m/%d"))
     input.click()
     page_session.wait_for_timeout(350)
     expect(page_session.get_by_role("menu")).to_be_visible()
-    today_button = page_session.get_by_role("button", name=today.strftime("%-d"), exact=True)
+    today_button = page_session.get_by_role("button", name=date.strftime("%-d"), exact=True)
     # We click it, but it does not trigger a change, so we don't auto close
     # Do we want to change this behaviour, and still close it?
     today_button.click()
     page_session.wait_for_timeout(350)
     expect(page_session.get_by_role("menu")).to_be_visible()
-    tomorrow_button = page_session.get_by_role("button", name=tomorrow.strftime("%-d"), exact=True)
+    tomorrow_button = page_session.get_by_role("button", name=date2.strftime("%-d"), exact=True)
     tomorrow_button.click()
     page_session.wait_for_timeout(350)
     expect(page_session.get_by_role("menu")).not_to_be_visible()
@@ -69,23 +69,23 @@ def test_input_date_range(solara_test, page_session: Page):
             on_value(value)
             set_value(value)
 
-        value, set_value = solara.use_state([today, tomorrow])
+        value, set_value = solara.use_state([date, date2])
         InputDateRange(value=value, label="label", on_value=update_value, classes=["test-class"])
 
     solara.display(Page())
 
     input = page_session.locator(".test-class input")
     expect(input).to_be_visible()
-    expect(input).to_have_value(f"{today.strftime('%Y/%m/%d')} - {tomorrow.strftime('%Y/%m/%d')}")
+    expect(input).to_have_value(f"{date.strftime('%Y/%m/%d')} - {date2.strftime('%Y/%m/%d')}")
     input.click()
     page_session.wait_for_timeout(350)
     expect(page_session.get_by_role("menu")).to_be_visible()
-    today_button = page_session.get_by_role("button", name=today.strftime("%-d"), exact=True)
+    today_button = page_session.get_by_role("button", name=date.strftime("%-d"), exact=True)
     today_button.click()
     page_session.wait_for_timeout(350)
     expect(page_session.locator(".test-class label")).to_contain_text("label (Please select two dates)")
     expect(page_session.get_by_role("menu")).to_be_visible()
-    tomorrow_button = page_session.get_by_role("button", name=tomorrow.strftime("%-d"), exact=True)
+    tomorrow_button = page_session.get_by_role("button", name=date2.strftime("%-d"), exact=True)
     tomorrow_button.click()
     page_session.wait_for_timeout(350)
     expect(page_session.locator(".test-class label")).not_to_contain_text("(Please select two dates)")
