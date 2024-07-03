81854c5e3b816d5b8d595a964601d6ec774b8990
Iisakki Rotko <iisakki.rotko@gmail.com>
2024-07-03 11:16:23 +0200

    Bump version: 1.33.0 → 1.34.0

--- packages/solara-enterprise/solara_enterprise/__init__.py
+++ packages/solara-enterprise/solara_enterprise/__init__.py
@@ -1,3 +1,3 @@
 "Enterprise features for Solara"
 
-__version__ = "1.33.0"
+__version__ = "1.34.0"
--- solara/server/static/solara_bootstrap.py
+++ solara/server/static/solara_bootstrap.py
@@ -119,7 +119,7 @@ async def main():
     ]
     for dep in requirements:
         await micropip.install(dep, keep_going=True)
-    await micropip.install("/wheels/solara-1.33.0-py2.py3-none-any.whl", keep_going=True)
+    await micropip.install("/wheels/solara-1.34.0-py2.py3-none-any.whl", keep_going=True)
     import solara
 
     el = solara.Warning("lala")
