6080fc482defa7f99b3448be9601e000193f3e63
lizzy985 <lizzy.yang985@gmail.com>
2024-11-12 21:13:27 -0700

    update code in cross_filter_dataframe-1

--- solara/website/pages/documentation/api/cross_filter/cross_filter_dataframe.py
+++ solara/website/pages/documentation/api/cross_filter/cross_filter_dataframe.py
@@ -13,11 +13,10 @@ df = plotly.data.gapminder()
 @solara.component
 def Page():
     solara.provide_cross_filter()
-    with solara.VBox() as main:
-        solara.CrossFilterReport(df, classes=["py-2"])
-        solara.CrossFilterSelect(df, "country")
-        solara.CrossFilterDataFrame(df)
-    return main
+
+    solara.CrossFilterReport(df, classes=["py-2"])
+    solara.CrossFilterSelect(df, "country")
+    solara.CrossFilterDataFrame(df)
 
 
 __doc__ += apidoc(solara.CrossFilterDataFrame.f)  # type: ignore
