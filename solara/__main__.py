widgetti/solara

commit 069a205c88a8cbcb0b0ca23f4d56889c8ad6134a

fix: do not crash if app is not a child of the current working directory

solara/__main__.py
@@ -341,7 +341,14 @@ def run(
         reload_excludes = restart_excludes if restart_excludes else []
         del restart_excludes
         reload_excludes = [str(solara_root / "website"), str(solara_root / "template")]
-        reload_excludes.append(app)
+        app_path = Path(app)
+        if app_path.exists():
+            # if app is not a child of the current working directory
+            # uvcorn crashes
+            if str(app_path.resolve()).startswith(str(Path.cwd().resolve())):
+                reload_excludes.append(str(app_path.resolve()))
+            print("reload_excludes", reload_excludes)
+        del app_path
         del solara_root
         reload = True
         # avoid sending many restarts

commit f49bffcda8d233b7f2f670409af52084f5d3bdaa

fix: wrong condition when determining which files to reload (#869)
069a205c88a8cbcb0b0ca23f4d56889c8ad6134a Caused the paths outside of working directory to be the only ones included instead of being excluded

solara/__main__.py
@@ -344,10 +344,9 @@ def run(
         app_path = Path(app)
         if app_path.exists():
             # if app is not a child of the current working directory
-            # uvcorn crashes
-            if str(app_path.resolve()).startswith(str(Path.cwd().resolve())):
+            # uvicorn crashes
+            if not str(app_path.resolve()).startswith(str(Path.cwd().resolve())):
                 reload_excludes.append(str(app_path.resolve()))
-            print("reload_excludes", reload_excludes)
         del app_path
         del solara_root
         reload = True