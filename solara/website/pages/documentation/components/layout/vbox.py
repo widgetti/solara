b31dbe0ac4d20169347a0c0dd646484deec2f66d
lizzy985 <lizzy.yang985@gmail.com>
2024-12-01 21:33:59 -0700

    docs update code vbox

--- solara/website/pages/documentation/components/layout/vbox.py
+++ solara/website/pages/documentation/components/layout/vbox.py
@@ -11,9 +11,8 @@ from ..common import ColorCard
 
 @solara.component
 def Page():
-    with solara.VBox() as main:
-        colors = "green red orange brown yellow pink".split()
-        with solara.VBox():
-            for color in colors:
-                ColorCard(color, color)
-    return main
+    colors = "green red orange brown yellow pink".split()
+    with solara.VBox():
+        for color in colors:
+            ColorCard(color, color)
+
