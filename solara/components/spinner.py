2d608aa3d412f1571152250729f25ee3067f70d8
Maarten Breddels <maartenbreddels@gmail.com>
2024-12-02 15:57:53 +0100

    feat: support colors for SpinnerSolara (#858)

--- solara/components/spinner.py
+++ solara/components/spinner.py
@@ -8,12 +8,15 @@ class SpinnerSolaraWidget(ipyvue.VueTemplate):
     template_file = (__file__, "spinner-solara.vue")
 
     size = traitlets.Unicode("64px").tag(sync=True)
+    color_back = traitlets.Unicode("#FFCF64").tag(sync=True)
+    color_front = traitlets.Unicode("#FF8C3E").tag(sync=True)
 
 
 @solara.component
-def SpinnerSolara(size="64px"):
+def SpinnerSolara(size="64px", color_back="#FFCF64", color_front="#FF8C3E"):
     """Spinner component with the Solara logo to indicate the app is busy.
 
+    ## Examples
     ### Basic example
 
     ```solara
@@ -24,7 +27,19 @@ def SpinnerSolara(size="64px"):
         solara.SpinnerSolara(size="100px")
     ```
 
+    ## Changing the colors
+    ```solara
+    import solara
+
+    @solara.component
+    def Page():
+        solara.SpinnerSolara(size="100px", color_back="Grey", color_front="Lime")
+    ```
+
+
     ## Arguments
      * `size`: Size of the spinner.
+     * `color_back`: Color of the spinner in the background.
+     * `color_front`: Color of the spinner in the foreground.
     """
-    return SpinnerSolaraWidget.element(size=size)
+    return SpinnerSolaraWidget.element(size=size, color_back=color_back, color_front=color_front)
