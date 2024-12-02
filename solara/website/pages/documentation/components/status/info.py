fba862aed11762bdfb6a325e16beb6b2a36da26a
lizzy985 <lizzy.yang985@gmail.com>
2024-12-01 22:10:00 -0700

    docs update code info

--- solara/website/pages/documentation/components/status/info.py
+++ solara/website/pages/documentation/components/status/info.py
@@ -21,7 +21,7 @@ def Page():
     dense = solara.use_reactive(False)
     outlined = solara.use_reactive(True)
     text = solara.use_reactive(True)
-  
+
     with solara.GridFixed(4):
         solara.Checkbox(label="Use icon", value=icon)
         solara.Checkbox(label="Show dense", value=dense)
@@ -34,7 +34,7 @@ def Page():
         outlined=outlined,
         icon=icon,
     )
- 
+
 
 
 __doc__ += apidoc(solara.Info.f)  # type: ignore
