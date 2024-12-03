ec3d816b26d27d893fbc66796e76e3920a6ca130
Li <94577240+lizzy985@users.noreply.github.com>
2024-12-03 02:20:42 -0700

    docs: update code cross_filter_slider (#864)
    * docs update code cross_filter_slider
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- solara/website/pages/documentation/api/cross_filter/cross_filter_slider.py
+++ solara/website/pages/documentation/api/cross_filter/cross_filter_slider.py
@@ -12,11 +12,9 @@ df = plotly.data.gapminder()
 @solara.component
 def Page():
     solara.provide_cross_filter()
-    with solara.VBox() as main:
-        solara.CrossFilterReport(df, classes=["py-2"])
-        solara.CrossFilterSlider(df, "pop", mode=">")
-        solara.CrossFilterSlider(df, "gdpPercap", mode="<")
-    return main
+    solara.CrossFilterReport(df, classes=["py-2"])
+    solara.CrossFilterSlider(df, "pop", mode=">")
+    solara.CrossFilterSlider(df, "gdpPercap", mode="<")
 
 
 __doc__ += apidoc(solara.CrossFilterSlider.f)  # type: ignore
