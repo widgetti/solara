e62def374854659846f57ff76123b1eea3a4f107
lizzy985 <lizzy.yang985@gmail.com>
2024-12-08 16:49:23 -0700

    fix code in warning-1

--- solara/website/pages/documentation/components/status/warning.py
+++ solara/website/pages/documentation/components/status/warning.py
@@ -17,30 +17,27 @@ from solara.website.utils import apidoc
 
 @solara.component
 def Page():
-    icon, set_icon = solara.use_state(True)
-    dense, set_dense = solara.use_state(False)
-    outlined, set_outlined = solara.use_state(True)
-    text, set_text = solara.use_state(True)
-
+    state = solara.use_reactive({
+        "icon": True,
+        "dense": False,
+        "outlined": True,
+        "text": True,
+    })
+    
     with solara.GridFixed(4):
-        solara.Checkbox(label="Use icon", value=icon, on_value=set_icon)
-        solara.Checkbox(label="Show dense", value=dense, on_value=set_dense)
-        solara.Checkbox(label="Show as text", value=text, on_value=set_text)
-        solara.Checkbox(label="Show outlined", value=outlined, on_value=set_outlined)
+        solara.Checkbox(label="Use icon", value=state["icon"], on_value=lambda val: state.update({"icon": val}))
+        solara.Checkbox(label="Show dense", value=state["dense"], on_value=lambda val: state.update({"dense": val}))
+        solara.Checkbox(label="Show as text", value=state["text"], on_value=lambda val: state.update({"text": val}))
+        solara.Checkbox(label="Show outlined", value=state["outlined"], on_value=lambda val: state.update({"outlined": val}))
+    
     solara.Warning(
-        f"This is solara.Warning(label='...', text={text}, dense={dense}, outlined={outlined}, icon={icon})",
-        text=text,
-        dense=dense,
-        outlined=outlined,
-        icon=icon,
+        f"This is solara.Warning(label='...', text={state['text']}, dense={state['dense']}, outlined={state['outlined']}, icon={state['icon']})",
+        text=state["text"],
+        dense=state["dense"],
+        outlined=state["outlined"],
+        icon=state["icon"],
     )
-    solara.Warning(
-        f"This is solara.Warning(label='...', text={text}, dense={dense}, outlined={outlined}, icon={icon})",
-        text=text,
-        dense=dense,
-        outlined=outlined,
-        icon=icon,
-    )
+
 
 
 __doc__ += apidoc(solara.Warning.f)  # type: ignore
