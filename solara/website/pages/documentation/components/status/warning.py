de7c693bb02f95497091a4fc830b6962300a7bad
lizzy985 <lizzy.yang985@gmail.com>
2024-12-08 17:15:46 -0700

    fix code in warning-3

--- solara/website/pages/documentation/components/status/warning.py
+++ solara/website/pages/documentation/components/status/warning.py
@@ -27,17 +27,17 @@ def Page():
     )
 
     with solara.GridFixed(4):
-        solara.Checkbox(label="Use icon", value=state["icon"], on_value=lambda val: state.value.update({"icon": val}))
-        solara.Checkbox(label="Show dense", value=state["dense"], on_value=lambda val: state.value.update({"dense": val}))
-        solara.Checkbox(label="Show as text", value=state["text"], on_value=lambda val: state.value.update({"text": val}))
-        solara.Checkbox(label="Show outlined", value=state["outlined"], on_value=lambda val: state.value.update({"outlined": val}))
+        solara.Checkbox(label="Use icon", value=state.value["icon"], on_value=lambda val: state.value.update({"icon": val}))
+        solara.Checkbox(label="Show dense", value=state.value["dense"], on_value=lambda val: state.value.update({"dense": val}))
+        solara.Checkbox(label="Show as text", value=state.value["text"], on_value=lambda val: state.value.update({"text": val}))
+        solara.Checkbox(label="Show outlined", value=state.value["outlined"], on_value=lambda val: state.value.update({"outlined": val}))
     
     solara.Warning(
         f"This is solara.Warning(label='...', text={state.value['text']}, dense={state.value['dense']}, outlined={state.value['outlined']}, icon={state.value['icon']})",
-        text=state.value.update["text"],
-        dense=state.value.update["dense"],
-        outlined=state.value.update["outlined"],
-        icon=state.value.update["icon"],
+        text=state.value["text"],
+        dense=state.value["dense"],
+        outlined=state.value["outlined"],
+        icon=state.value["icon"],
     )
 
 
