widgetti/solara

commit dd621c145d1dbc3e6b8519588a790349c94994f3

perf: throttle sending websocket messages (#400)
This gives a better performance for starlette.
enable with SOLARA_EXPERIMENTAL_PERFORMANCE=1

solara/server/starlette.py
@@ -74,15 +74,33 @@ class WebsocketWrapper(websocket.WebsocketWrapper):
     def __init__(self, ws: starlette.websockets.WebSocket, portal: anyio.from_thread.BlockingPortal) -> None:
         self.ws = ws
         self.portal = portal
+        self.to_send: List[Union[str, bytes]] = []
+        self.task = asyncio.ensure_future(self.process_messages_task())
+
+    async def process_messages_task(self):
+        while True:
+            await asyncio.sleep(0.05)
+            while len(self.to_send) > 0:
+                first = self.to_send.pop(0)
+                if isinstance(first, bytes):
+                    await self.ws.send_bytes(first)
+                else:
+                    await self.ws.send_text(first)
 
     def close(self):
         self.portal.call(self.ws.close)
 
     def send_text(self, data: str) -> None:
-        self.portal.call(self.ws.send_text, data)
+        if settings.main.experimental_performance:
+            self.to_send.append(data)
+        else:
+            self.portal.call(self.ws.send_bytes, data)
 
     def send_bytes(self, data: bytes) -> None:
-        self.portal.call(self.ws.send_bytes, data)
+        if settings.main.experimental_performance:
+            self.to_send.append(data)
+        else:
+            self.portal.call(self.ws.send_bytes, data)
 
     async def receive(self):
         if hasattr(self.portal, "start_task_soon"):
@@ -179,8 +197,6 @@ async def kernel_connection(ws: starlette.websockets.WebSocket):
     await ws.accept()
 
     def websocket_thread_runner(ws: starlette.websockets.WebSocket, portal: anyio.from_thread.BlockingPortal):
-        ws_wrapper = WebsocketWrapper(ws, portal)
-
         async def run():
             try:
                 assert session_id is not None
@@ -200,6 +216,7 @@ async def kernel_connection(ws: starlette.websockets.WebSocket):
     # each websocket however, is handled from a separate thread
     try:
         async with anyio.from_thread.BlockingPortal() as portal:
+            ws_wrapper = WebsocketWrapper(ws, portal)
             thread_return = anyio.to_thread.run_sync(websocket_thread_runner, ws, portal)
             await thread_return
     finally:

solara/server/settings.py
@@ -151,6 +151,7 @@ class MainSettings(BaseSettings):
     base_url: str = ""  # e.g. https://myapp.solara.run/myapp/
     platform: str = sys.platform
     host: str = HOST_DEFAULT
+    experimental_performance: bool = False
 
     class Config:
         env_prefix = "solara_"

commit fa8d8b22f8e4d0acfadd6805caa918af7fb23762

fix: send str using send_bytes in websocket for starlette server
.send_text called .send_bytes

Bug introduced in #400, released since v1.25.0

This showed up when deploying in Modal with the error:
```
Traceback (most recent call last):
  File "/pkg/modal/_runtime/container_io_manager.py", line 764, in handle_input_exception
    yield
  File "/pkg/modal/_container_entrypoint.py", line 210, in run_input_async
    await generator_output_task  # Wait to finish sending generator outputs.
    ^^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "/pkg/synchronicity/async_wrap.py", line 29, in wrapper
    return await user_wrapper(*args, **kwargs)
           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "/pkg/synchronicity/async_wrap.py", line 29, in wrapper
    return await user_wrapper(*args, **kwargs)
           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "/pkg/synchronicity/async_wrap.py", line 32, in wrapper
    raise uc_exc.exc
  File "/pkg/modal/_runtime/container_io_manager.py", line 549, in generator_output_task
    messages_bytes = [serialize_data_format(message, data_format)]
                      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "/pkg/modal/_serialization.py", line 346, in serialize_data_format
    return _serialize_asgi(obj).SerializeToString(deterministic=True)
           ^^^^^^^^^^^^^^^^^^^^
  File "/pkg/modal/_serialization.py", line 217, in _serialize_asgi
    websocket_send=api_pb2.Asgi.WebsocketSend(
                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^
TypeError: expected bytes, str found
```

solara/server/starlette.py
@@ -159,7 +159,7 @@ class WebsocketWrapper(websocket.WebsocketWrapper):
             if settings.main.experimental_performance:
                 self.to_send.append(data)
             else:
-                self.portal.call(self._send_bytes_exc, data)  # type: ignore
+                self.portal.call(self._send_text_exc, data)  # type: ignore
 
     def send_bytes(self, data: bytes) -> None:
         if self.portal is None:
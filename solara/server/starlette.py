fa8d8b22f8e4d0acfadd6805caa918af7fb23762
Maarten A. Breddels <maartenbreddels@gmail.com>
2024-12-16 09:54:50 +0100

    fix: send str using send_bytes in websocket for starlette server
    .send_text called .send_bytes
    
    Bug introduced in #400, released since v1.25.0
    
    This showed up when deploying in Modal with the error:
    ```
    Traceback (most recent call last):
      File "/pkg/modal/_runtime/container_io_manager.py", line 764, in handle_input_exception
        yield
      File "/pkg/modal/_container_entrypoint.py", line 210, in run_input_async
        await generator_output_task  # Wait to finish sending generator outputs.
        ^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/pkg/synchronicity/async_wrap.py", line 29, in wrapper
        return await user_wrapper(*args, **kwargs)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/pkg/synchronicity/async_wrap.py", line 29, in wrapper
        return await user_wrapper(*args, **kwargs)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/pkg/synchronicity/async_wrap.py", line 32, in wrapper
        raise uc_exc.exc
      File "/pkg/modal/_runtime/container_io_manager.py", line 549, in generator_output_task
        messages_bytes = [serialize_data_format(message, data_format)]
                          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/pkg/modal/_serialization.py", line 346, in serialize_data_format
        return _serialize_asgi(obj).SerializeToString(deterministic=True)
               ^^^^^^^^^^^^^^^^^^^^
      File "/pkg/modal/_serialization.py", line 217, in _serialize_asgi
        websocket_send=api_pb2.Asgi.WebsocketSend(
                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^
    TypeError: expected bytes, str found
    ```

--- solara/server/starlette.py
+++ solara/server/starlette.py
@@ -159,7 +159,7 @@ class WebsocketWrapper(websocket.WebsocketWrapper):
             if settings.main.experimental_performance:
                 self.to_send.append(data)
             else:
-                self.portal.call(self._send_bytes_exc, data)  # type: ignore
+                self.portal.call(self._send_text_exc, data)  # type: ignore
 
     def send_bytes(self, data: bytes) -> None:
         if self.portal is None:
