ebff57d8952f9db501f9d13e9fae65cbfaf08bdf
Riley Thai <rileythai@proton.me>
2025-02-11 15:14:47 +1100

    feat: FigureBokeh docs page
    - now a page in lab

---
+++ solara/website/pages/documentation/components/lab/figurebokeh.py
@@ -0,0 +1,17 @@
+"""
+# FigureBokeh
+
+Display a Bokeh figure.
+
+"""
+
+import solara
+from solara.website.components import NoPage
+from solara.website.utils import apidoc
+
+title = "FigureBokeh"
+
+__doc__ += apidoc(
+    solara.lab.components.figurebokeh.FigureBokeh.f)  # type: ignore
+
+Page = NoPage
