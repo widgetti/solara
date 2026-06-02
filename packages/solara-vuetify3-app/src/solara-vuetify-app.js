bdef45399cb9697b85ff5f4613a814b79fbee051
Mario Buikhuizen <mariobuikhuizen@widgetti.io>
2026-06-02 15:09:07 +0200

    fix: import all lab components

--- packages/solara-vuetify3-app/src/solara-vuetify-app.js
+++ packages/solara-vuetify3-app/src/solara-vuetify-app.js
@@ -3,10 +3,14 @@ import * as Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 
 import * as components from 'vuetify/components';
+import * as labComponents from 'vuetify/labs/components';
 import * as directives from 'vuetify/directives';
 
 const vuetifyPlugin = Vuetify.createVuetify({
-    components,
+    components: {
+        ...components,
+        ...labComponents,
+    },
     directives,
 });
 
