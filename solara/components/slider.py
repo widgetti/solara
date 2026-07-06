9ba3e500a8ce9e4cdb5d040071c5ba338b317e3b
Mario Buikhuizen <mariobuikhuizen@widgetti.io>
2026-07-06 17:42:39 +0200

    fix: make SliderValue compatible with Vuetify 3

--- solara/components/slider.py
+++ solara/components/slider.py
@@ -331,6 +331,7 @@ def SliderValue(
     index, set_index = solara.use_state(values.index(reactive_value.value), key="index")
 
     def on_index(index):
+        index = int(index)
         set_index(index)
         value = values[index]
         reactive_value.set(value)
@@ -341,10 +342,11 @@ def SliderValue(
             v_model=index,
             on_v_model=on_index,
             show_ticks=True,
-            ticks=values,
+            ticks={index: str(value) for index, value in enumerate(values)},
             label=label,
             min=0,
             max=len(values) - 1,
+            step=1,
             density="default",
             hide_details=True,
             disabled=disabled,
